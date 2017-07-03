8fcd8bfbb177e75a70d2c599963c1f679315f4be
saikrishnatangallapally <saikrishna.ronaldo7@gmail.com>
2017-07-04 02:47:11 +0530

    databasetest

---
+++ dbtest/DataBaseTest/app/src/main/java/com/example/saikrishna/databasetest/Student.java
@@ -0,0 +1,22 @@
+package com.example.saikrishna.databasetest;
+
+/**
+ * Created by sai krishna on 7/4/2017.
+ */
+
+public class Student {
+    private int _id;
+    private int rollno;
+    public int getId() {
+        return _id;
+    }
+    public void setId(int _id) {
+        this._id = _id;
+    }
+    public int getRollno() {
+        return rollno;
+    }
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+}
---
+++ dbtest/DataBaseTest/app/src/main/java/com/example/saikrishna/databasetest/Testdb.java
@@ -0,0 +1,133 @@
+package com.example.saikrishna.databasetest;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import java.io.IOException;
+import java.util.List;
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class Testdb extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private final static String TAG = "MainActivity";
+    DatabaseHelper dbHelper= null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testdb);
+        dbHelper = new DatabaseHelper(this, getFilesDir().getAbsolutePath());
+        try {
+            dbHelper.prepareDatabase();
+        } catch (IOException e) {
+            Log.e(TAG, e.getMessage());
+        }
+        showData();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void showData() {
+        List<Student> list = dbHelper.getEmployees();
+        StringBuffer data = new StringBuffer();
+        for (int i =0; i< list.size(); i++) {
+            Student stud = list.get(i);
+            data.append(stud.getId()).append(",")
+                    .append(",").append(stud.getRollno()).append("<br/>");
+        }
+        TextView textView = (TextView)findViewById(R.id.bodytext);
+        textView.setText(Html.fromHtml(data.toString()));
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.testdb, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
